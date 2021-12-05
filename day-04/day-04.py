da3d9ddf40eda9c013e93de58d5c3d5f1d09d714
pazzobarbone <johnluis@hotmail.it>
2021-12-05 01:08:44 +0000

    First attempt of refactoring

--- day-04/day-04.py
+++ day-04/day-04.py
@@ -1,98 +1,115 @@
 from pathlib import Path
 import numpy as np
 
+
+def get_board_combinations_and_associated_board_ids(lines):
+    combinations = list()
+    board_ids = list()
+    current_board_combinations = list()
+    current_board_id = 0
+    if lines[-1] != "\n":
+        lines.append("\n")
+    for line in lines:
+        line = line.strip()
+        if not line:
+            if current_board_combinations:
+                for c in range(len(current_board_combinations[0])):
+                    combinations.append(
+                        [current_board_combinations[r][c] for r in range(len(current_board_combinations))]
+                    )
+                    board_ids.append(current_board_id)
+                current_board_combinations = []
+                current_board_id += 1
+        else:
+            combinations.append([int(i) for i in line.split()])
+            current_board_combinations.append([int(i) for i in line.split()])
+            board_ids.append(current_board_id)
+    return combinations, board_ids
+
+
+def find_first_bingo_board_combinations_and_last_drawn_number(combinations, board_ids, drawn_numbers):
+    last_number_drawn = None
+    first_bingo_board_id = None
+    scores = [0] * len(combinations)
+    number_of_boards = len(set(board_ids))
+    combinations = np.array(combinations)
+    done = False
+    for d in drawn_numbers:
+        matches = np.where(combinations == d)
+        for m in matches[0]:
+            scores[m] += 1
+            if scores[m] == len(combinations[0]):
+                first_bingo_board_id = board_ids[m]
+                last_number_drawn = d
+                done = True
+                break
+        if done:
+            break
+    combinations_per_board = int(len(combinations) / number_of_boards)
+    bingo_board_combinations = combinations[
+        combinations_per_board * first_bingo_board_id : combinations_per_board * first_bingo_board_id
+        + combinations_per_board
+    ]
+    return bingo_board_combinations, last_number_drawn
+
+
+def calculate_answer(board_combinations, last_drawn_number):
+    combination_length = len(board_combinations[0])
+    unmarked_number_sum = 0
+    last_number_drawn_idx = drawn_numbers.index(last_drawn_number)
+    extraction = drawn_numbers[: last_number_drawn_idx + 1]
+    for r in range(combination_length):
+        for c in range(combination_length):
+            if not board_combinations[r][c] in extraction:
+                unmarked_number_sum += board_combinations[r][c]
+    print(unmarked_number_sum * last_drawn_number)
+
+
+def find_last_bingo_board_combinations_and_last_drawn_number(combinations, board_ids, drawn_numbers):
+    last_number_drawn = None
+    last_bingo_board_id = None
+    scores = [0] * len(combinations)
+    number_of_boards = max(board_ids) + 1
+    won = [0] * number_of_boards
+    rows_in_a_board = len(combinations[0])
+    done = False
+    combinations = np.array(combinations)
+    for d in drawn_numbers:
+        matches = np.where(combinations == d)
+        for m in matches[0]:
+            scores[m] += 1
+            board_id = board_ids[m]
+            if (scores[m] == rows_in_a_board) and (not won[board_id]):
+                won[board_id] = 1
+                if sum(won) == len(combinations) / 2 / rows_in_a_board:
+                    last_bingo_board_id = board_id
+                    last_number_drawn = d
+                    done = True
+                    break
+        if done:
+            break
+    combinations_per_board = int(len(combinations) / number_of_boards)
+    bingo_board_combinations = combinations[
+        combinations_per_board * last_bingo_board_id : combinations_per_board * last_bingo_board_id
+        + combinations_per_board
+    ]
+    return bingo_board_combinations, last_number_drawn
+
+
 this_file = Path(__file__)
 with open(this_file.parent / "input.txt") as file:
     lines = file.readlines()
 
 drawn_numbers = [int(i) for i in lines[0].strip().split(",")]
 lines.remove(lines[0])
-all_board_lines = list()
-this_board_lines = list()
-this_board_n = 0
-board_ids = list()
-if lines[-1] != "\n":
-    lines.append("\n")
-for line in lines:
-    line = line.strip()
-    if not line:
-        if this_board_lines:
-            for c in range(len(this_board_lines[0])):
-                all_board_lines.append([this_board_lines[r][c] for r in range(len(this_board_lines))])
-                board_ids.append(this_board_n)
-            this_board_lines = []
-            this_board_n += 1
-    else:
-        all_board_lines.append([int(i) for i in line.split()])
-        this_board_lines.append([int(i) for i in line.split()])
-        board_ids.append(this_board_n)
+combinations, board_ids = get_board_combinations_and_associated_board_ids(lines)
 
-boards = np.array(all_board_lines)
+first_bingo_board, last_drawn_number = find_first_bingo_board_combinations_and_last_drawn_number(
+    combinations, board_ids, drawn_numbers
+)
+calculate_answer(first_bingo_board, last_drawn_number)
 
-
-done = False
-scores = [0] * len(all_board_lines)
-last_number_drawn = None
-rows_in_a_board = len(all_board_lines[0])
-for d in drawn_numbers:
-    matches = np.where(boards == d)
-    for m in matches[0]:
-        scores[m] += 1
-        if scores[m] == rows_in_a_board:
-            winning_board_n = board_ids[m]
-            last_number_drawn = d
-            done = True
-            break
-    if done:
-        break
-
-
-winning_board = all_board_lines[
-    2 * rows_in_a_board * winning_board_n : 2 * rows_in_a_board * winning_board_n + 2 * rows_in_a_board
-]
-columns_in_a_board = rows_in_a_board
-unmarked_number_sum = 0
-last_number_drawn_idx = drawn_numbers.index(last_number_drawn)
-extraction = drawn_numbers[: last_number_drawn_idx + 1]
-for r in range(rows_in_a_board):
-    for c in range(columns_in_a_board):
-        if not winning_board[r][c] in extraction:
-            unmarked_number_sum += winning_board[r][c]
-print(unmarked_number_sum * last_number_drawn)
-
-
-done = False
-scores = [0] * len(all_board_lines)
-number_of_boards = max(board_ids) + 1
-won = [0] * number_of_boards
-last_number_drawn = None
-rows_in_a_board = len(all_board_lines[0])
-for d in drawn_numbers:
-    matches = np.where(boards == d)
-    for m in matches[0]:
-        scores[m] += 1
-        board_id = board_ids[m]
-        if (scores[m] == rows_in_a_board) and (not won[board_id]):
-            won[board_id] = 1
-            if sum(won) == len(all_board_lines) / 2 / rows_in_a_board:
-                last_winning_board_n = board_id
-                last_number_drawn = d
-                done = True
-                break
-    if done:
-        break
-
-
-winning_board_n = last_winning_board_n
-winning_board = all_board_lines[
-    2 * rows_in_a_board * winning_board_n : 2 * rows_in_a_board * winning_board_n + 2 * rows_in_a_board
-]
-columns_in_a_board = rows_in_a_board
-unmarked_number_sum = 0
-last_number_drawn_idx = drawn_numbers.index(last_number_drawn)
-extraction = drawn_numbers[: last_number_drawn_idx + 1]
-for r in range(rows_in_a_board):
-    for c in range(columns_in_a_board):
-        if not winning_board[r][c] in extraction:
-            unmarked_number_sum += winning_board[r][c]
-print(unmarked_number_sum * last_number_drawn)
+last_bingo_board, last_drawn_number = find_last_bingo_board_combinations_and_last_drawn_number(
+    combinations, board_ids, drawn_numbers
+)
+calculate_answer(last_bingo_board, last_drawn_number)
