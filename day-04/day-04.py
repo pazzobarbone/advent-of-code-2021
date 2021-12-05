ead3f5ebc335297f5da8624014b35c568bf9ea54
pazzobarbone <johnluis@hotmail.it>
2021-12-05 02:01:55 +0000

    First attempt of refactoring

--- day-04/day-04.py
+++ day-04/day-04.py
@@ -2,114 +2,96 @@ from pathlib import Path
 import numpy as np
 
 
-def get_board_combinations_and_associated_board_ids(lines):
+class Board:
+    def __init__(self, id, combinations):
+        self._id = id
+        self._bingo = False
+        self._combinations = np.array(combinations)
+        self._score = [0] * len(combinations)
+        self._combination_length = len(combinations[0])
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def bingo(self):
+        return self._bingo
+
+    def check_number(self, number):
+        matches = np.where(self._combinations == number)
+        for m in matches[0]:
+            self._score[m] += 1
+            if self._score[m] == len(self._combinations[0]):
+                self._bingo = True
+                break
+
+    def get_puzzle_answer(self, draws):
+        unmarked_number_sum = 0
+        for r in range(self._combination_length):
+            for c in range(self._combination_length):
+                if not self._combinations[r][c] in draws:
+                    unmarked_number_sum += self._combinations[r][c]
+        return unmarked_number_sum * draws[-1]
+
+
+def create_board_list(lines):
+    boards = list()
     combinations = list()
-    board_ids = list()
-    current_board_combinations = list()
-    current_board_id = 0
+    id = 1
     if lines[-1] != "\n":
         lines.append("\n")
     for line in lines:
         line = line.strip()
         if not line:
-            if current_board_combinations:
-                for c in range(len(current_board_combinations[0])):
-                    combinations.append(
-                        [current_board_combinations[r][c] for r in range(len(current_board_combinations))]
-                    )
-                    board_ids.append(current_board_id)
-                current_board_combinations = []
-                current_board_id += 1
+            if combinations:
+                boards.append(Board(id, combinations))
+                id += 1
+                combinations = []
         else:
             combinations.append([int(i) for i in line.split()])
-            current_board_combinations.append([int(i) for i in line.split()])
-            board_ids.append(current_board_id)
-    return combinations, board_ids
+    return boards
 
 
-def find_first_bingo_board_combinations_and_last_drawn_number(combinations, board_ids, drawn_numbers):
-    last_number_drawn = None
-    first_bingo_board_id = None
-    scores = [0] * len(combinations)
-    number_of_boards = len(set(board_ids))
-    combinations = np.array(combinations)
+def part1(boards, draws):
     done = False
-    for d in drawn_numbers:
-        matches = np.where(combinations == d)
-        for m in matches[0]:
-            scores[m] += 1
-            if scores[m] == len(combinations[0]):
-                first_bingo_board_id = board_ids[m]
-                last_number_drawn = d
+    for d in draws:
+        for board in boards:
+            board.check_number(d)
+            if board.bingo:
+                drawn_so_far = draws[: draws.index(d) + 1]
+                print(board.get_puzzle_answer(drawn_so_far))
                 done = True
                 break
         if done:
             break
-    combinations_per_board = int(len(combinations) / number_of_boards)
-    bingo_board_combinations = combinations[
-        combinations_per_board * first_bingo_board_id : combinations_per_board * first_bingo_board_id
-        + combinations_per_board
-    ]
-    return bingo_board_combinations, last_number_drawn
 
 
-def calculate_answer(board_combinations, last_drawn_number):
-    combination_length = len(board_combinations[0])
-    unmarked_number_sum = 0
-    last_number_drawn_idx = drawn_numbers.index(last_drawn_number)
-    draw = drawn_numbers[: last_number_drawn_idx + 1]
-    for r in range(combination_length):
-        for c in range(combination_length):
-            if not board_combinations[r][c] in draw:
-                unmarked_number_sum += board_combinations[r][c]
-    print(unmarked_number_sum * last_drawn_number)
-
-
-def find_last_bingo_board_combinations_and_last_drawn_number(combinations, board_ids, drawn_numbers):
-    last_number_drawn = None
-    last_bingo_board_id = None
-    scores = [0] * len(combinations)
-    number_of_boards = max(board_ids) + 1
-    won = [0] * number_of_boards
-    rows_in_a_board = len(combinations[0])
+def part2(boards, draws):
     done = False
-    combinations = np.array(combinations)
-    for d in drawn_numbers:
-        matches = np.where(combinations == d)
-        for m in matches[0]:
-            scores[m] += 1
-            board_id = board_ids[m]
-            if (scores[m] == rows_in_a_board) and (not won[board_id]):
-                won[board_id] = 1
-                if sum(won) == len(combinations) / 2 / rows_in_a_board:
-                    last_bingo_board_id = board_id
-                    last_number_drawn = d
+    bingos = [0] * len(boards)
+    for d in draws:
+        for i, board in enumerate(boards):
+            board.check_number(d)
+            if board.bingo:
+                bingos[i] = 1
+                if sum(bingos) == len(boards):
+                    drawn_so_far = draws[: draws.index(d) + 1]
+                    print(board.get_puzzle_answer(drawn_so_far))
                     done = True
                     break
         if done:
             break
-    combinations_per_board = int(len(combinations) / number_of_boards)
-    bingo_board_combinations = combinations[
-        combinations_per_board * last_bingo_board_id : combinations_per_board * last_bingo_board_id
-        + combinations_per_board
-    ]
-    return bingo_board_combinations, last_number_drawn
 
 
 this_file = Path(__file__)
 with open(this_file.parent / "input.txt") as file:
     lines = file.readlines()
 
-drawn_numbers = [int(i) for i in lines[0].strip().split(",")]
+draws = [int(i) for i in lines[0].strip().split(",")]
 lines.remove(lines[0])
-combinations, board_ids = get_board_combinations_and_associated_board_ids(lines)
 
-first_bingo_board, last_drawn_number = find_first_bingo_board_combinations_and_last_drawn_number(
-    combinations, board_ids, drawn_numbers
-)
-calculate_answer(first_bingo_board, last_drawn_number)
-
-last_bingo_board, last_drawn_number = find_last_bingo_board_combinations_and_last_drawn_number(
-    combinations, board_ids, drawn_numbers
-)
-calculate_answer(last_bingo_board, last_drawn_number)
+# boards = create_board_list(lines)
+# part1(boards, draws)
+boards = create_board_list(lines)
+part2(boards, draws)
